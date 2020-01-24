84f6d66fc2996821272c34e221bb56179bbb5e11
Prashant Singh <iprashant2402@gmail.com>
2020-01-24 13:02:23 +0530

    Updates

---
+++ precache-manifest.b487d296b40e1d9401fba26b73007ba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b7b28b7230d7e3b34966e9c748070d",
+    "url": "/doorzyDashboard/index.html"
+  },
+  {
+    "revision": "5b31717d3e6b4c409b79",
+    "url": "/doorzyDashboard/static/css/main.655cc48f.chunk.css"
+  },
+  {
+    "revision": "324fb646f5856ed67003",
+    "url": "/doorzyDashboard/static/js/2.3f9a1504.chunk.js"
+  },
+  {
+    "revision": "5b31717d3e6b4c409b79",
+    "url": "/doorzyDashboard/static/js/main.1acf3ba2.chunk.js"
+  },
+  {
+    "revision": "63ebed5c37a76815d389",
+    "url": "/doorzyDashboard/static/js/runtime~main.dcf4054b.js"
+  },
+  {
+    "revision": "486401b562bdb8d6b93f18780033184f",
+    "url": "/doorzyDashboard/static/media/logo_1.486401b5.png"
+  },
+  {
+    "revision": "0ef8dae3961cce619f10d0e49a0240ec",
+    "url": "/doorzyDashboard/static/media/logo_alternate_plain.0ef8dae3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/doorzyDashboard/precache-manifest.f01534c83f6b0ec456f487df2889edd8.js"
+  "/doorzyDashboard/precache-manifest.b487d296b40e1d9401fba26b73007ba0.js"
 );
 
 self.addEventListener('message', (event) => {
